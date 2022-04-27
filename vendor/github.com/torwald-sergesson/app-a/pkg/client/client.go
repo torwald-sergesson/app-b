cf69e9c3a40ad870430254c201e09bd331bceb33
Vladimir.Kasatkin <Vladimir.Kasatkin@acronis.com>
2022-04-27 10:26:56 +0300

    Add vendor

---
+++ vendor/github.com/torwald-sergesson/app-a/pkg/client/client.go
@@ -0,0 +1,70 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/torwald-sergesson/app-a/pkg/dto"
+)
+
+type Client struct {
+	addr       string
+	httpClient http.Client
+}
+
+func NewClient(addr string, timeout time.Duration) *Client {
+	return &Client{
+		addr: addr,
+		httpClient: http.Client{
+			Transport:     nil,
+			CheckRedirect: nil,
+			Jar:           nil,
+			Timeout:       timeout,
+		},
+	}
+}
+
+func (cli *Client) url(path string) (url.URL, error) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   cli.addr,
+		Path:   path,
+	}
+	return u, nil
+}
+
+func (cli *Client) Me() (dto.User, error) {
+	u, err := cli.url("/api/me")
+	if err != nil {
+		return dto.User{}, fmt.Errorf("fail to build url: %w", err)
+	}
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return dto.User{}, fmt.Errorf("fail to build request: %w", err)
+	}
+	log.Printf("request; method=%s; url=%s\n", req.Method, req.URL.String())
+	resp, err := cli.httpClient.Do(req)
+	if err != nil {
+		return dto.User{}, fmt.Errorf("fail to do request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return dto.User{}, fmt.Errorf("fail to read body: %w", err)
+	}
+	log.Printf("body: %s\n", body)
+	if resp.StatusCode != http.StatusOK {
+		return dto.User{}, fmt.Errorf("wrong status code; expected=%d; received=%d", http.StatusOK, resp.StatusCode)
+	}
+	var user dto.User
+	if err = json.Unmarshal(body, &user); err != nil {
+		return dto.User{}, fmt.Errorf("fail to unmarshal body: %w", err)
+	}
+	return user, nil
+}
